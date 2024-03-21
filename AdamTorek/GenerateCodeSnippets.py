9b89edf32f357c724fabedf23550e183e86a0e2e
Adam-Torek <adamtorek@u.boisestate.edu>
2024-03-20 21:41:54 -0600

    got results from starcoder2-7B for mbpp and humaneval datasets

--- AdamTorek/GenerateCodeSnippets.py
+++ AdamTorek/GenerateCodeSnippets.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 import transformers
 import torch
-from evalplus.data import get_human_eval_plus, write_jsonl
+from evalplus.data import get_mbpp_plus, get_human_eval_plus, write_jsonl
 
 def generate_code(prompt, model, tokenizer, device):
         inputs = tokenizer.encode(prompt, return_tensors="pt").to(device)
@@ -16,20 +16,24 @@ def generate_code(prompt, model, tokenizer, device):
                          max_length=1024)
         return tokenizer.decode(outputs[0])
 
+def gen_dataset_samples(dataset, model, tokenizer, device):
+    return [
+        dict(task_id=task_id, solution=generate_code(problem["prompt"], model, tokenizer, device))
+        for task_id, problem in dataset.items()
+    ]
+
 def main():
     device = "cuda"
-    model_id = "WizardLM/WizardCoder-3B-V1.0"
+    model_id = "bigcode/starcoder2-7b"
 
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16).to(device)
 
-    samples = [
-        dict(task_id=task_id, solution=generate_code(problem["prompt"], model, tokenizer, device))
-        for task_id, problem in get_human_eval_plus().items()
-    ]
-    output_name = "samples_" + model_id.split("/")[1] + ".jsonl"
-    write_jsonl(output_name, samples)
+    humaneval_results = gen_dataset_samples(get_human_eval_plus(), model, tokenizer, device)
+    mbpp_results = gen_dataset_samples(get_mbpp_plus(), model, tokenizer, device)
 
+    write_jsonl("humaneval_" + model_id.split("/")[1] + "" + ".jsonl", humaneval_results)
+    write_jsonl("mbpp_" + model_id.split("/")[1] + "" + ".jsonl", mbpp_results)
 
 if __name__ == "__main__":
     main()
